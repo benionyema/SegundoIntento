6728be616932f884e381cfcb0783861bd2f0bdb4
Benedit <benielamo@gmail.com>
2024-08-25 20:09:04 +0100

    Frase y suma

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ namespace Borrador
     {
         static void Main()
         {
-
+            Console.WriteLine("Hola Benedit");
+            int a = 5, b = 6;
+            Console.WriteLine(a + b);
         }
     }
 }
