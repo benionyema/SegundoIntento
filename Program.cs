c4f2c11f34f1e52b985c3bd8d367dc3250c7fb2c
Benedit <benielamo@gmail.com>
2024-08-26 17:15:45 +0100

    Commit de fallo en Github

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Borrador
             int a = 5, b = 6;
             Console.WriteLine(a + b);
             Console.WriteLine(a - b);
+            Console.WriteLine("Se subió el código correctamente");
         }
     }
 }
